abe2cd2b5111fce481a6b03c4f91db075586a1ab
weiwenhao <1101140857@qq.com>
2026-01-12 19:04:41 +0800

    Update leibniz.go

--- src/leibniz.go
+++ src/leibniz.go
@@ -1,23 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"strconv"
 	"strings"
+	"fmt"
 )
 
 func main() {
 	file, _ := os.ReadFile("rounds.txt")
 	rounds, _ := strconv.Atoi(strings.TrimSpace(string(file)))
-
 	x := 1.0
 	pi := 1.0
-	stop := float64(rounds + 2)
+	stop := rounds + 2
 
-	for i := 2.0; i <= stop; i++ {
+	for i := 2; i <= stop; i++ {
 		x = -x
-		pi += x / (2.0*i - 1.0)
+		pi += x / float64(2*i - 1)
 	}
 
 	pi *= 4.0
